739430f7f18e1fd783b83d4b2c715b41537fe751
LTH <johtta888@naver.com>
2022-07-06 11:09:43 +0900

    Feat: 수정 _app title 태그

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({Component, pageProps}: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Persona</title>
+        <title>title</title>
       </Head>
       <Reset />
       <Component {...pageProps} />
